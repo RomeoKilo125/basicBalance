d4de6f26d00ee9f17a80f6c4484063aa2b37ba33
RomeoKilo125 <RomeoKilo125@gmail.com>
2022-06-05 19:56:48 -0400

    removed a couple things to cleanup warnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-              <h1>BasicBalance</h1>
+      <h1>BasicBalance</h1>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const {ApolloServer} = require('apollo-server-express')
+// const { ApolloServer } = require('apollo-server-express')
 const path = require('path')
 
 const PORT = process.env.port || 3001
 const app = express()
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
